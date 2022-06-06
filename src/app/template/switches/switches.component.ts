99258c74ad7b72d8125003159ca27484642f91d2
unknown <lamica.olivares@gmail.com>
2022-06-06 09:09:12 -0400

    switches-radios-checkbox

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  persona = {
+    genero: 'F',
+    notificaciones: true,
   }
-
+  terminosYcondiciones: boolean = false;
 }
